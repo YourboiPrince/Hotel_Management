79811149d27f4de2bcbe62c561b62a6c6e6885dd
morio <princebandit2004@gmail.com>
2024-04-25 22:10:06 +0300

    firestore commit

--- lib/presentation/authentication/screens/signup_screen.dart
+++ lib/presentation/authentication/screens/signup_screen.dart
@@ -1,7 +1,5 @@
-// ignore_for_file: use_build_context_synchronously
-
 import 'package:flutter/material.dart';
-import 'package:hotel/home.dart';
+import 'package:hotel/presentation/authentication/screens/login_screen.dart'; // Import login screen
 import 'package:hotel/presentation/authentication/widgets/logo.dart';
 import 'package:provider/provider.dart';
 import 'package:hotel/providers/auth_provider.dart';
@@ -31,57 +29,65 @@ class _CreateUserScreenState extends State<SignUpScreen> {
       appBar: AppBar(
         title: const Text('Sign Up'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            const LogoWidget(),
-            TextFormField(
-              controller: _displayNameController,
-              decoration: const InputDecoration(
-                labelText: 'displayName',
-              ),
+      body: Center(
+        child: Container(
+          width: 450,
+          decoration: const BoxDecoration(
+            color: Colors.purple, // Set the background color to purple
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                const LogoWidget(),
+                TextFormField(
+                  controller: _displayNameController,
+                  decoration: const InputDecoration(
+                    labelText: 'displayName',
+                  ),
+                ),
+                TextFormField(
+                  controller: _emailController,
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
+                  ),
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: _passwordController,
+                  decoration: const InputDecoration(
+                    labelText: 'Password',
+                  ),
+                  obscureText: true,
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    String email = _emailController.text.trim();
+                    String password = _passwordController.text.trim();
+                    String displayName = _displayNameController.text.trim();
+                    // Pass the current context to the AuthProvider
+                    await context
+                       .read<AuthProvider>()
+                       .createUserWithEmailAndPassword(
+                            context, email, password, displayName);
+                    // Check if the user is created successfully
+                    if (context.read<AuthProvider>().user!= null) {
+                      Navigator.pushReplacement(context,
+                          MaterialPageRoute(builder: (context) => const LoginScreen()));
+                    }
+                  },
+                  child: const Text('Sign Up'),
+                ),
+                //add a button to navigate to the login screen
+                TextButton(
+                  onPressed: () {
+                    Navigator.pushReplacementNamed(context, '/login');
+                  },
+                  child: const Text('Already have an account? Login'),
+                ),
+              ],
             ),
-            TextFormField(
-              controller: _emailController,
-              decoration: const InputDecoration(
-                labelText: 'Email',
-              ),
-            ),
-            const SizedBox(height: 16),
-            TextFormField(
-              controller: _passwordController,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-              ),
-              obscureText: true,
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                String email = _emailController.text.trim();
-                String password = _passwordController.text.trim();
-                String displayName = _displayNameController.text.trim();
-                // Pass the current context to the AuthProvider
-                await context
-                    .read<AuthProvider>()
-                    .createUserWithEmailAndPassword(
-                        context, email, password, displayName);
-                // Check if the user is created successfully
-                if (context.read<AuthProvider>().user != null) {
-                  Navigator.pushReplacement(context,
-                      MaterialPageRoute(builder: (context) => const MyHomePage()));
-                }
-              },
-              child: const Text('Sign Up'),
-            ),
-            //add a button to navigate to the login screen
-            TextButton(
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, '/login');
-              },
-              child: const Text('Already have an account? Login'),
-            ),
-          ],
+          ),
         ),
       ),
     );
